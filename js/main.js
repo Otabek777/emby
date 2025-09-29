db53d1ac71fc6351621661072592aa7b4f589d2c
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2025-09-30 02:45:39 +0500

    new edit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+// Lenis
+const lenis = new Lenis({
+    wrapper: document.querySelector('.scroll-container'),
+    lerp: 0.1,
+    duration: 1.2,
+    smoothTouch: true
+});
+lenis.on('scroll', e => {
+    console.log(e);
+});
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+// Animate on Scroll
+const animatedElements = document.querySelectorAll('.o-title_line');
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+};
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('is-inview');
+            observer.unobserve(entry.target);
+        } else {
+            entry.target.classList.remove('is-inview');
+        }
+    });
+});
+animatedElements.forEach(el => observer.observe(el));
+
 $(".questions__btn").click(function() {
     $(this).toggleClass("active");
 });
